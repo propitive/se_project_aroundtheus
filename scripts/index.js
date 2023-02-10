f5c6c8480d8d920061d3e1679cc2f05276c5e823
Propitive <propitive@gmail.com>
2023-02-10 00:19:58 -0600

    Cards are made from template with data from array

--- scripts/index.js
+++ scripts/index.js
@@ -42,3 +42,57 @@ closeButton.addEventListener("click", function () {
   console.log("Modal has been closed");
 });
 //END - Modal box toggle
+
+// START - Form fields
+let profileName = document.querySelector(".profile-info__name");
+let profileSubtitle = document.querySelector(".profile-info__subtitle");
+let inputName = document.querySelector(".modal__name");
+let inputDescription = document.querySelector(".modal__description");
+
+function toggleForm() {
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileSubtitle.textContent;
+}
+
+editButton.addEventListener("click", function () {
+  toggleForm();
+});
+//END - Form fields
+
+//START - Editing your name and about me
+let profileFormElement = document.querySelector(".modal");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileSubtitle.textContent = inputDescription.value;
+}
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+profileFormElement.addEventListener("submit", function () {
+  modal.classList.remove("modal__open");
+  console.log("Modal has been closed");
+});
+//END - Editing your name and about me
+
+//START: Rendering cards
+let galleryCards = document.querySelector(".gallery__cards");
+
+function getCardElement(data) {
+  console.log(data);
+  let cardTemplate = document.querySelector("#card").content;
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardTitle = data.name;
+  const cardImage = data.link;
+  cardElement.querySelector(".card__image").src = cardImage;
+  cardElement.querySelector(".card__image").alt = cardTitle;
+  cardElement.querySelector(".card__title").textContent = cardTitle;
+  return cardElement;
+}
+
+for (let i = 0; i <= initialCards.length; i++) {
+  const data = initialCards[i];
+  const card = getCardElement(data);
+  galleryCards.append(card);
+}
+//END: Rendering cards
