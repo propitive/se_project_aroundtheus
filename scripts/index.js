e4b74b634b928a31eccbb38b5549aacc137a4362
Jose M. Maldonado <jmaldonado@associa.us>
2023-03-07 17:34:32 -0600

    I just need to add <span> to the other three inputs

--- scripts/index.js
+++ scripts/index.js
@@ -199,6 +199,8 @@ newItemModalForm.addEventListener("submit", handleCardFormSubmit);
 
 /* const profileFormElement = profileModal.querySelector(".modal__form"); */
 /* const inputName = document.querySelector(".modal__name"); */
+
+/*
 const inputNameErrorMessage = profileFormElement.querySelector(`.${inputName.id}-error`)
 
 inputName.addEventListener("input", function(evt) {
@@ -226,3 +228,56 @@ const isValid = () => {
 };
 
 inputName.addEventListener("input", isValid);
+*/
+
+const inputNameErrorMessage = profileFormElement.querySelector(`.${inputName.id}-error`)
+
+inputName.addEventListener("input", function(evt) {
+  console.log(evt.target.validity.valid)
+})
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+  inputElement.classList.add("form__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+  inputElement.classList.remove("form__input_type_error");
+  errorElement.classList.remove("form__input-error_active");
+  errorElement.textContent = "";
+}
+
+const isValid = (formElement, inputElement) => {
+  if (!inputName.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      isValid(formElement, inputElement)
+    })
+  })
+}
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".form"));
+
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
