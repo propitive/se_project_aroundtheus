16884ace63eaa4d5a616686277b9dcc820b0543f
Propitive <propitive@gmail.com>
2023-03-25 15:52:06 -0500

    Popup.js rough draft & working on PopupWithForm.js

---
+++ scripts/Popup.js
@@ -0,0 +1,48 @@
+// import { closeModal, closeModalOnRemoteClick, keyHandler } from "./utils";
+
+import { closeModal } from "./utils";
+
+export default class Popup {
+  constructor({ popupSelector }) {
+    this._popupElement = document.querySelector(popupSelector);
+  }
+
+  open() {
+    // open popup
+    const modal = this._popupElement.querySelector(".modal");
+
+    modal.classList.add("modal__open");
+    modal.addEventListener("mousedown", this.setEventListeners);
+    document.addEventListener("keydown", this._handleEscClose);
+  }
+
+  close() {
+    // closes popup
+    const modal = this._popupElement.querySelector(".modal");
+
+    modal.classList.remove("modal__open");
+    modal.removeEventListener("mousedown", this.setEventListeners);
+    document.removeEventListener("keydown", this._handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    // listens for esc button
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+
+  setEventListeners(evt) {
+    // sets event listeners
+    const modal = this._popupElement.querySelector(".modal");
+    const modalCloseButton = modal.querySelector(".close-icon");
+
+    modalCloseButton.addEventListener("click", () => {
+      this.close();
+    });
+
+    if (evt.target === evt.currentTarget) {
+      this.close();
+    }
+  }
+}
