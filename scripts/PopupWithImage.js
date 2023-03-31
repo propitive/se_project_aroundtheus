637d2d8943bb07d268362db21b4beff8b7134dd0
Propitive <propitive@gmail.com>
2023-03-31 17:05:23 -0500

    working on PopupWithImage

--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -3,13 +3,13 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super({ popupSelector });
-    this._imagePopUpTitle = this._popupElement.querySelector(
-      ".image-pop-up__title"
-    );
   }
 
   open(name, link) {
     this._image = document.querySelector(".image-pop-up__image");
+    this._imagePopUpTitle = this._popupElement.querySelector(
+      ".image-pop-up__title"
+    );
 
     this._imagePopUpTitle.textContent = name;
     this._image.src = link;
