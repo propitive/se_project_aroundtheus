a251f918f3321ea5a56946ffef9f88e635a0db11
Propitive <propitive@gmail.com>
2023-04-04 15:54:13 -0500

    trying to connect css in index.js + rearranged some files per reviewer

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,14 @@ import {
   inputName,
   inputDescription,
   addButton,
-} from "./constants.js";
-import Card from "./Card.js";
-import FormValidator from "./FormValidator.js";
-import Section from "./Section.js";
-import PopupWithImage from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
+} from "../scripts/constants.js";
+import Card from "../scripts/Card.js";
+import FormValidator from "../scripts/FormValidator.js";
+import Section from "../scripts/Section.js";
+import PopupWithImage from "../scripts/PopupWithImage.js";
+import PopupWithForm from "../scripts/PopupWithForm.js";
+import UserInfo from "../scripts/UserInfo.js";
+import "./index.css";
 
 const editFormValidator = new FormValidator(
   formConfig,
