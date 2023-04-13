11d8055cc6fe3d9cc5ea5dd4eb0b6c991c52f642
Propitive <propitive@gmail.com>
2023-04-12 22:00:57 -0500

    created the Api class and nothing else

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,21 +3,77 @@ export default class Api {
     (this._baseUrl = baseUrl), (this._headers = headers);
   }
 
+  _checkResponse(res) {
+    if (res) {
+      return res.json();
+    }
+
+    return Promise.reject(`Error ${res.status}`);
+  }
+
   getUserInfo() {
     return fetch(`${this._baseUrl}/users/me`, {
       method: "GET",
       headers: this._headers,
-    });
+    }).then(this._checkResponse);
   }
 
   getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
       method: "GET",
       headers: this._headers,
-    });
+    }).then(this._checkResponse);
   }
 
   getAPIInfo() {
     return Promise.all([this.getUserInfo(), this.getInitialCards()]);
+  }
+
+  updateProfileInfo(userData) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: userData.name,
+        about: userData.about,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  addNewCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({ name, link }),
+    }).then(this._checkResponse);
+  }
+
+  deleteUserCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  addCardLikes(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "PUT",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  removeCardLikes(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  updateProfileAvatar(avatar) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({ avatar }),
+    }).then(this._checkResponse);
   }
 }
